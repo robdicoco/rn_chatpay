f335d853fc62cce8a27d7662700704177a9cada1
robdicoco <pavusa@gmail.com>
2025-06-12 22:36:40 -0300

    eeee

--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,20 @@ const { getDefaultConfig } = require("expo/metro-config");
 const {
   withLibsodiumResolver,
 } = require("@burnt-labs/abstraxion-react-native/metro.libsodium");
+// import {   withLibsodiumResolver,
+// } from '@burnt-labs/abstraxion-react-native/metro.libsodium';
 
 const config = getDefaultConfig(__dirname);
 config.resolver.unstable_enablePackageExports = false;
 module.exports = withLibsodiumResolver(config);
+
+
+// // metro.config.js
+// import { getDefaultConfig } from "expo/metro-config/src/index.js"; // Updated path with .js
+// import {
+//   withLibsodiumResolver,
+// } from "@burnt-labs/abstraxion-react-native/metro.libsodium.js"; // Updated path with .js
+
+// const config = getDefaultConfig(__dirname);
+// config.resolver.unstable_enablePackageExports = false;
+// export default withLibsodiumResolver(config);
