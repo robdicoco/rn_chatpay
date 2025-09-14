06be5b8444ea3429c8f051cda2ff56a408be0f87
robdicoco <pavusa@gmail.com>
2025-09-14 18:16:02 -0300

    chore: update libsodium resolver configuration in metro.config.js and upgrade @burnt-labs/abstraxion-react-native to version 1.0.0-alpha.8

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,13 @@
 // metro.config.js
 const { getDefaultConfig } = require("expo/metro-config");
-const {
-  withLibsodiumResolver,
-} = require("@burnt-labs/abstraxion-react-native/metro.libsodium");
 
 const config = getDefaultConfig(__dirname);
 config.resolver.unstable_enablePackageExports = false;
-module.exports = withLibsodiumResolver(config);
+
+// Add libsodium resolver manually
+config.resolver.alias = {
+  ...config.resolver.alias,
+  'libsodium-wrappers': 'react-native-libsodium',
+};
+
+module.exports = config;
